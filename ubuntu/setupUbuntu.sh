5985b42a6169eabf42599e2bd365c552550a5c6a
zajac1 <adam.zajac90@gmail.com>
2015-03-26 13:53:34 +0100

    Create setupUbuntu.sh
    Added script to speed up seting up devel environment on freshly installed Ubuntu

---
+++ ubuntu/setupUbuntu.sh
@@ -0,0 +1,23 @@
+add-apt-repository "deb http://dl.google.com/linux/chrome/deb/ stable main";
+add-apt-repository "deb http://repository-origin.spotify.com stable non-free";
+add-apt-repository "ppa:numix/ppa";
+apt-get install git;
+apt-get install spotify;
+apt-get install nodejs;
+apt-get install ngrok-server;
+apt-get install google-chrome-stable;
+apt-get install numix-gtk-theme;
+apt-get install numix-icons;
+apt-get install tmux;
+apt-get install php5-cli;
+apt-get install imagemagick;
+apt-get install python3;
+apt-get install python-pip
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/zajac1/dotfiles/tree/master/ubuntu/.bashrc
+git clone https://github.com/zajac1/dotfiles/tree/master/ubuntu/.vimrc
+
+
+
+###TOOLS
+#CSSDig
