64f895a8fa73be23b87558af245723e45da01282
Adam Zając <adam.zajac90@gmail.com>
2024-02-05 10:39:19 +0100

    barbar config refactor

--- nvim/after/plugin/barbar.lua
+++ nvim/after/plugin/barbar.lua
@@ -1,27 +1,12 @@
-local map = vim.api.nvim_set_keymap
-local opts = { noremap = true, silent = true }
-
-map('n', 'gT', '<Cmd>BufferPrevious<CR>', opts)
-map('n', 'gt', '<Cmd>BufferNext<CR>', opts)
--- Re-order to previous/next
-map('n', 'g<', '<Cmd>BufferMovePrevious<CR>', opts)
-map('n', 'g>', '<Cmd>BufferMoveNext<CR>', opts)
--- Goto buffer in position...
-map('n', 'g1', '<Cmd>BufferGoto 1<CR>', opts)
-map('n', 'g2', '<Cmd>BufferGoto 2<CR>', opts)
-map('n', 'g3', '<Cmd>BufferGoto 3<CR>', opts)
-map('n', 'g4', '<Cmd>BufferGoto 4<CR>', opts)
-map('n', 'g5', '<Cmd>BufferGoto 5<CR>', opts)
-map('n', 'g6', '<Cmd>BufferGoto 6<CR>', opts)
-map('n', 'g7', '<Cmd>BufferGoto 7<CR>', opts)
-map('n', 'g8', '<Cmd>BufferGoto 8<CR>', opts)
-map('n', 'g9', '<Cmd>BufferGoto 9<CR>', opts)
-map('n', 'g0', '<Cmd>BufferLast<CR>', opts)
--- Pin/unpin buffer
-map('n', 'gp', '<Cmd>BufferPin<CR>', opts)
--- Close buffer
-map('n', 'gx', '<Cmd>BufferClose<CR>', opts)
--- Close all but current buffer
-map('n', 'gX', '<Cmd>BufferCloseAllButCurrentOrPinned<CR>', opts)
--- Magic buffer-picking mode
-map('n', 'g;', '<Cmd>BufferPick<CR>', opts)
+vim.keymap.set('n', 'gT', vim.cmd.BufferPrevious, { desc = 'Go to previous tab' })
+vim.keymap.set('n', 'gt', vim.cmd.BufferNext, { desc = 'Go to next tab' })
+vim.keymap.set('n', 'g<', vim.cmd.BufferMovePrevious, { desc = 'Reorder tab to previous' })
+vim.keymap.set('n', 'g>', vim.cmd.BufferMoveNext, { desc = 'Reorder tab to next' })
+vim.keymap.set('n', 'gp', vim.cmd.BufferPin, { desc = 'Pin tab' })
+vim.keymap.set('n', 'gx', vim.cmd.BufferClose, { desc = 'Close tab' })
+vim.keymap.set('n', 'gX', vim.cmd.BufferCloseAllButCurrentOrPinned, { desc = 'Close all tabs except pinned and current one' })
+vim.keymap.set('n', 'g;', vim.cmd.BufferPick, { desc = 'Tab-picking mode' })
+for i = 1, 9 do
+  vim.keymap.set('n', 'g' .. i, function() vim.cmd.BufferGoto(i) end, { desc = 'Go to tab number ' .. i })
+end
+vim.keymap.set('n', 'g0', vim.cmd.BufferLast, { desc = 'Go to the last tab' })
