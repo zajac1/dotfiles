c9ef8265b9d61ecd770053a60b6f2d3bdf74af4e
Adam Zając <adam.zajac90@gmail.com>
2025-01-30 20:36:54 +0100

    More telescope usage, fix double code action prompts

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -32,3 +32,4 @@ vim.keymap.set('n', '<leader>p', builtin.git_files, { desc = 'Omnisearch for all
 vim.keymap.set('n', '<leader>f', builtin.live_grep, { desc = 'Grep through *all* files' })
 vim.keymap.set('n', '<leader>s', function() builtin.git_status({ previewer = delta, layout_strategy = 'vertical' }) end, { desc = 'List of files according to git status' })
 vim.keymap.set('n', '<leader>r', telescope.extensions.recent_files.pick, { desc = 'List of recent files' })
+vim.keymap.set("n", "<leader>h", builtin.keymaps, { desc = 'Show Telescope menu for all available keybindings' })
